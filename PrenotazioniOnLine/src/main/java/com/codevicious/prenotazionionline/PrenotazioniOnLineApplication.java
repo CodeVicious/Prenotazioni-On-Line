5eaa496503e6efc7a7e0b5e1d7045b92b70e8936
Administrator <Administrator@DESKTOP-FMM75VD.sanminiato.it>
2018-11-08 17:55:28 +0100

    Added CORS Filter

--- PrenotazioniOnLine/src/main/java/com/codevicious/prenotazionionline/PrenotazioniOnLineApplication.java
+++ PrenotazioniOnLine/src/main/java/com/codevicious/prenotazionionline/PrenotazioniOnLineApplication.java
@@ -1,8 +1,13 @@
 package com.codevicious.prenotazionionline;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.ws.rs.client.Client;
 
 import org.eclipse.jetty.server.session.SessionHandler;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import org.skife.jdbi.v2.DBI;
@@ -70,6 +75,21 @@ public class PrenotazioniOnLineApplication extends Application<PrenotazioniOnLin
 		 * errorResource = new ErrorResource();
 		 * environment.jersey().register(errorResource);
 		 */
+		
+		
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
+		
 		environment.getApplicationContext().setSessionHandler(new SessionHandler());
 
 		environment.jersey().setUrlPattern("/api/*");
